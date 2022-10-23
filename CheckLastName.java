ec84730cc0698e3be458626befc01e9954c70fd0
shubhammane8668 <shubhsmane@gmail.com>
2022-10-23 15:49:47 +0530

    added

---
+++ CheckLastName.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.Regex;
+
+public class CheckLastName {
+
+}
---
+++ Main.java
@@ -0,0 +1,17 @@
+
+package com.bridgelabz.Regex;
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserValid userValid = new UserValid();
+        userValid.checkFirstName();
+        userValid.checkLastName();
+        userValid.checkValidMail();
+        userValid.checkMobileNumber();
+        userValid.checkPassword();
+        userValid.checkPasswordWithAtLeastOneUpperCase();
+        userValid.passwordWithOneNumericNum();
+        userValid.passwordWithOneSpecialCharacter();
+    }
+}
